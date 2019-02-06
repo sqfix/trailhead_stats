b9c6768d3921f9f8a65238c91c1357a80fec00ee
Max Zvychayny <nordskol1@gmail.com>
2019-02-06 17:08:53 +0200

    POST request is working for one record per time

--- flaskMapping.py
+++ flaskMapping.py
@@ -1,22 +1,40 @@
-from flask import Flask
+import datetime
 import requests
+import json
+from flask import Flask, request
 from lxml import html
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():    
-    return get_user_info()
+@app.route('/', methods=['POST'])
+def index():
+    if request.method == 'POST':
+        user_ids = request.json['user_ids']
+        return get_user_info(str(user_ids))
+    return ''
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 
-def get_user_info():
-    page = requests.get("https://trailhead.salesforce.com/en/me/00550000006xt96AAA")
+def get_user_info(user_id):
+    page = requests.get("https://trailhead.salesforce.com/en/me/" + user_id)
+    print(user_id)
     tree = html.fromstring(page.content)
-    prices = tree.xpath('//span[@class="user-information__achievements-data"]/text()')
-    return prices
+    badges = tree.xpath('//div[@class="user-information__achievements-data"]/text()')[0]
+    points = tree.xpath('//div[@class="user-information__achievements-data"]/text()')[1]
 
+    return str(UserStats(badges, points, datetime.date.today(), user_id))
+
+
+class UserStats(object):
+    def __init__(self, badges, points, date, trail_id):
+        self.badges = badges
+        self.points = points
+        self.date = date
+        self.trailId = trail_id
+
+    def __str__(self):
+        return self.badges + ' ' + self.points + ' ' + self.trailId
